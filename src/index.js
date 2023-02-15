c65bd764f4b7b01109c25153773fa3f842fea2a1
rachel-eckert <110422541+rachel-eckert@users.noreply.github.com>
2023-02-15 15:54:54 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./store/index.js";
 import Main from "./Main";
-/* Import and destructure main from src/component/index.js
-and anything else you may need here */
 
 const container = document.getElementById("root");
 const root = createRoot(container);
