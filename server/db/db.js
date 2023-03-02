5ddc4c3c414a59ffba1e7e45c86f6bbcfa43fa9d
Rachel Eckert <rachel.eckert13@gmail.com>
2023-03-02 13:24:42 -0500

    fixed more db issues

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,14 @@
 const chalk = require("chalk");
 const Sequelize = require("sequelize");
 const pkg = require("../../package.json");
-
+const config = {
+  logging: false,
+};
 const dbName = process.env.NODE_ENV === "test" ? `${pkg.name}-test` : pkg.name;
 console.log(chalk.yellow(`Opening database connection to ${dbName}`));
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
   config
 );
 
