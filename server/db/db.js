d21fa000174cdccacfc1ceba3578798e50e8358f
Rachel Eckert <rachel.eckert13@gmail.com>
2023-03-02 13:32:39 -0500

    trying new things for render

--- server/db/db.js
+++ server/db/db.js
@@ -4,9 +4,15 @@ const pkg = require("../../package.json");
 const config = {
   logging: false,
 };
-const dbName = process.env.NODE_ENV === "test" ? `${pkg.name}-test` : pkg.name;
+const dbName = pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
 console.log(chalk.yellow(`Opening database connection to ${dbName}`));
-
+if (process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
   config
