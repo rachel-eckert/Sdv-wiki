314cf2fd508dde5548b5a983e7be6b6c7c29d399
Rachel Eckert <rachel.eckert13@gmail.com>
2023-03-02 13:22:59 -0500

    more db changes

--- server/db/db.js
+++ server/db/db.js
@@ -5,8 +5,9 @@ const pkg = require("../../package.json");
 const dbName = process.env.NODE_ENV === "test" ? `${pkg.name}-test` : pkg.name;
 console.log(chalk.yellow(`Opening database connection to ${dbName}`));
 
-const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
-  logging: false,
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  config
+);
 
 module.exports = db;
