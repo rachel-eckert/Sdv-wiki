513befced2c86fc273c35cf5a0f0a9794523c6ef
Rachel Eckert <rachel.eckert13@gmail.com>
2023-03-02 14:13:38 -0500

    got rid of volleyball

--- server/app.js
+++ server/app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const volleyball = require("volleyball");
+
 const app = express();
 app.use("/api", require("./api")); // include our routes!
 // static middleware
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(cors());
-app.use(volleyball);
 
 app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
